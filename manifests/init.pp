f505a3739a3c90eee71bf4b121656737ae2ba54f
Gavin Riggi <gavin.riggi@wellspring.com>
2024-02-20 14:35:44 -0500

    change: provider to puppet_gem

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
 # Installs dependencies for awssm functions
 class awssm {
-  stdlib::ensure_packages(['aws-sdk-secretsmanager'], { provider => 'gem' })
+  stdlib::ensure_packages(['aws-sdk-secretsmanager'], { provider => 'puppet_gem' })
 }
